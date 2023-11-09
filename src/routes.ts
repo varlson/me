eec76151942609b29ea9a5ec9222355bd17253c6
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2023-11-09 11:42:50 -0300

    update removing google

--- src/routes.ts
+++ src/routes.ts
@@ -22,41 +22,41 @@ router.get("/envs", (req: Request, res: Response) => {
   });
 });
 
-router.get("/month-status", async (req: Request, res: Response) => {
-  const { googleSheets, auth, spreadsheetId } = await getGoogleAuthSheets();
-  // const { googleSheets, auth, spreadsheetId } = await getGoogleAuthSheets();
+// router.get("/month-status", async (req: Request, res: Response) => {
+//   const { googleSheets, auth, spreadsheetId } = await getGoogleAuthSheets();
+//   // const { googleSheets, auth, spreadsheetId } = await getGoogleAuthSheets();
 
-  try {
-    const _balance = await googleSheets.spreadsheets.values.get({
-      auth,
-      spreadsheetId,
-      range: "api!D2:E5",
-    });
+//   try {
+//     const _balance = await googleSheets.spreadsheets.values.get({
+//       auth,
+//       spreadsheetId,
+//       range: "api!D2:E5",
+//     });
 
-    // const mes = _balance.data.values[0][1];
-    // const saldoInicial = _balance.data.values[1][1];
-    // const despesa = _balance.data.values[2][1];
-    // const saldoAtual = _balance.data.values[3][1];
+//     // const mes = _balance.data.values[0][1];
+//     // const saldoInicial = _balance.data.values[1][1];
+//     // const despesa = _balance.data.values[2][1];
+//     // const saldoAtual = _balance.data.values[3][1];
 
-    const [
-      [label1, totalOfMonth],
-      [label2, totalExpenses],
-      [label3, balance],
-      [label4, currentMonth],
-    ] = _balance.data.values as any;
+//     const [
+//       [label1, totalOfMonth],
+//       [label2, totalExpenses],
+//       [label3, balance],
+//       [label4, currentMonth],
+//     ] = _balance.data.values as any;
 
-    return res.status(200).json({
-      status: true,
-      //   datas: balance.data,
-      datas: { currentMonth, totalOfMonth, totalExpenses, balance },
-    });
-  } catch (error: any) {
-    return res.status(501).json({
-      status: false,
-      error: error.message,
-    });
-  }
-});
+//     return res.status(200).json({
+//       status: true,
+//       //   datas: balance.data,
+//       datas: { currentMonth, totalOfMonth, totalExpenses, balance },
+//     });
+//   } catch (error: any) {
+//     return res.status(501).json({
+//       status: false,
+//       error: error.message,
+//     });
+//   }
+// });
 
 router.post("/data", async (req: Request, res: Response) => {
   const { data } = req.body;
